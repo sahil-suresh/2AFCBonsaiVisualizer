35aab05e84abdf9f0627fc5aefde6ebbfbbc9947
Sahil Suresh <67023799+sahil-suresh@users.noreply.github.com>
2022-05-17 17:52:30 -0400

    Add files via upload

---
+++ Extensions/ResponseId.cs
@@ -0,0 +1,11 @@
+public enum ResponseId
+{
+    None,
+    Hit,
+    Miss,
+    FalseAlarm,
+    CorrectRejection,
+    PullPenalty,
+    IncorrectAction,
+    EarlyResponse
+}
---
+++ Extensions/StateId.cs
@@ -0,0 +1,26 @@
+public enum StateId
+{
+    ITI,
+    Left,
+    Right,
+    Response,
+    Timeout,
+
+    Annotation,
+
+    Joystick,
+    Lick,
+    LickLeft,
+    LickRight,
+    LickCenter,
+    Blink,
+
+
+    Hit,
+    Miss,
+    FalseAlarm,
+    CorrectRejection,
+    IncorrectAction,
+    PullPenalty,
+    EarlyResponse
+}
